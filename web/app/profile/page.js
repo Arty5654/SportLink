04299d16804d46fec1312abc360c274f91521bb0
AllenChang7 <allenychang2003@gmail.com>
2023-10-01 17:39:44 -0400

    changes

--- web/app/profile/page.js
+++ web/app/profile/page.js
@@ -8,16 +8,13 @@ const ProfilePage = () => {
         <div className="flex flex-col gap-2">
           <p>My Info</p>
           <p>Edit Profile</p>
-          <p>Password and Security</p>
-          <p>Notification Settings</p>
           <p>Settings</p>
         </div>
       </div>
 
       {/* ITEM: Main Info*/}
-      <div className="w-2/3 text-left">
-        <h1 className="font-base text-3xl">Allen Chang</h1>
-        <p>Profile Rating:</p>
+      <div className="w-2/3 text-left font-base text-3xl">
+        <h1>Allen Chang</h1>
       </div>
     </div>
   );
