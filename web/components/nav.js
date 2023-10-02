d576ce20accc5c4fb7e5424e975c2af4647cf125
Yash Mehta <imyash@live.com>
2023-10-01 22:58:07 -0400

    added links to navbar to respective pages

--- web/components/nav.js
+++ web/components/nav.js
@@ -1,15 +1,18 @@
 import React from "react";
+import Link from "next/link";
 
 const Nav = () => {
   return (
     <nav className="w-full bg-white text-black h-12 border-b border-black px-32 flex items-center justify-between">
       <div className="flex gap-2">
         <div className="relative group">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M9 6.75V15m6-6v8.25m.503 3.498l4.875-2.437c.381-.19.622-.58.622-1.006V4.82c0-.836-.88-1.38-1.628-1.006l-3.869 1.934c-.317.159-.69.159-1.006 0L9.503 3.252a1.125 1.125 0 00-1.006 0L3.622 5.689C3.24 5.88 3 6.27 3 6.695V19.18c0 .836.88 1.38 1.628 1.006l3.869-1.934c.317-.159.69-.159 1.006 0l4.994 2.497c.317.158.69.158 1.006 0z" />
-          </svg>
+          <Link href="/maps">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M9 6.75V15m6-6v8.25m.503 3.498l4.875-2.437c.381-.19.622-.58.622-1.006V4.82c0-.836-.88-1.38-1.628-1.006l-3.869 1.934c-.317.159-.69.159-1.006 0L9.503 3.252a1.125 1.125 0 00-1.006 0L3.622 5.689C3.24 5.88 3 6.27 3 6.695V19.18c0 .836.88 1.38 1.628 1.006l3.869-1.934c.317-.159.69-.159 1.006 0l4.994 2.497c.317.158.69.158 1.006 0z" />
+            </svg>
 
-          <span className="absolute top-full mt-1 mb-2 left-1/2 transform -translate-x-1/2 px-2 py-1 text-sm text-white bg-black rounded opacity-0 group-hover:opacity-100 transition duration-500 whitespace-nowrap">Maps</span>
+            <span className="absolute top-full mt-1 mb-2 left-1/2 transform -translate-x-1/2 px-2 py-1 text-sm text-white bg-black rounded opacity-0 group-hover:opacity-100 transition duration-500 whitespace-nowrap">Maps</span>
+          </Link>
         </div>
 
         <div className="relative group">
@@ -21,9 +24,12 @@ const Nav = () => {
         </div>
 
       </div>
-      <div className="flex gap text-3xl font-semibold">
-        <p className="text-green-500">Sport</p><p className="text-blue-500">Link</p>
-      </div>
+      <Link href="/">
+        <div className="flex gap text-3xl font-semibold">
+          <p className="text-green-500">Sport</p><p className="text-blue-500">Link</p>
+        </div>
+      </Link>
+      
       <div className="flex gap-2">
         <div className="relative group">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
@@ -34,11 +40,13 @@ const Nav = () => {
         </div>
         
         <div className="relative group">
+          <Link href="/profile">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
           </svg>
           
           <span className="absolute top-full mt-1 mb-2 left-1/2 transform -translate-x-1/2 px-2 py-1 text-sm text-white bg-black rounded opacity-0 group-hover:opacity-100 transition duration-500 whitespace-nowrap">Your Account</span>
+          </Link>
         </div>
       </div>
     </nav>
