ef43b784aa1708f4f9e5e24d79e952b78708c1ba
Arty Avetissian <102695236+Arty5654@users.noreply.github.com>
2023-10-24 20:37:03 -0400

    Update page.js
    Nicer UI Implemented

--- web/app/profile/search/page.js
+++ web/app/profile/search/page.js
@@ -19,7 +19,7 @@ function UserLookupPage() {
     try {
       setLoading(true);
       const response = await axios.get(`http://localhost:5000/user_lookup?searchTerm=${searchTerm}`);
-      setSearchResults(response.data); // Assuming the response contains the matching user data
+      setSearchResults(response.data); 
     } catch (error) {
       console.error('Error searching for users', error);
     } finally {
@@ -33,32 +33,48 @@ function UserLookupPage() {
   }, []);
 
   return (
-    <div className="w-full flex pb-64">
-      <div className="w-1/4">
-        <Sidebar active="search" />
-      </div>
-      <div className="w-3/4">
-        <h1>User Lookup</h1>
-        <input
-          type="text"
-          placeholder="Enter username, email, or phone number"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
-        <button onClick={handleSearch} disabled={loading}>
-          Search
+    <div className="flex h-screen">
+      <Sidebar active="search" />
+
+      <div className="w-3/4 p-8">
+        <h1 className="text-2xl font-semibold mb-4">User Lookup</h1>
+
+        <div className="mb-4">
+          <input
+            type="text"
+            placeholder="Enter username, email, or phone number"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            className="w-full p-2 border rounded-lg focus:outline-none focus:border-blue-500"
+          />
+        </div>
+
+        <button
+          onClick={handleSearch}
+          disabled={loading}
+          className="bg-blue-500 text-white py-2 px-4 rounded-lg cursor-pointer"
+        >
+          {loading ? 'Searching...' : 'Search'}
         </button>
 
-        {loading && <p>Loading...</p>}
+        {searchResults.length === 0 && !loading && (
+          <p className="mt-4 text-gray-600">No results found.</p>
+        )}
 
-        <div>
-          {searchResults.length === 0 && !loading && <p>No results found.</p>}
+        <div className="mt-4 space-y-4">
           {searchResults.map((user) => (
-            <div key={user.id}>
-              <p>Name: {user.name}</p>
-              <p>Username: {user.username}</p>
-              <p>Email: {user.email}</p>
-              <p>Phone: {user.phone}</p>
+            <div key={user.id} className="bg-white shadow rounded-lg p-4">
+              <img
+                src={ProfileImage}
+                alt="Profile"
+                className="w-16 h-16 rounded-full object-cover"
+              />
+              <div>
+                <p className="text-lg font-semibold">{user.name}</p>
+                <p className="text-gray-600">Username: {user.username}</p>
+                <p className="text-gray-600">Email: {user.email}</p>
+                <p className="text-gray-600">Phone: {user.phone}</p>
+              </div>
             </div>
           ))}
         </div>
