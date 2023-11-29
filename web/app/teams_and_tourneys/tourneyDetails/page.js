e85ecceee892172abeabfcf4ec5f151eb266be6c
Arty Avetissian <102695236+Arty5654@users.noreply.github.com>
2023-11-29 01:01:07 -0500

    Update page.js
    Can now join tournys

--- web/app/teams_and_tourneys/tourneyDetails/page.js
+++ web/app/teams_and_tourneys/tourneyDetails/page.js
@@ -3,13 +3,20 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import User from "@app/User";
 
+
 const TournamentDetails = () => {
   const [user, setUser] = useState(new User());
+  const [selectedTeamId, setSelectedTeamId] = useState(null);
+  const [isJoinTournamentModalOpen, setIsJoinTournamentModalOpen] = useState(false);
+  const [newTeamName, setNewTeamName] = useState('');
+  const [userTeams, setUserTeams] = useState([])
   const [tournament, setTournament] = useState({
     sport: "",
     teamCount: "",
     duration: 0,
-    matchDuration: ""
+    matchDuration: "",
+    teams: [],
+    isFull: false
   });
 
   useEffect(() => {
@@ -28,11 +35,14 @@ const TournamentDetails = () => {
         try {
           const detailsResponse = await axios.get(`http://localhost:5000/get_tournament_details?id=${tournamentId}`);
           setTournament({
+            objectID: detailsResponse.data._id,
             sport: detailsResponse.data.sport,
             teamCount: detailsResponse.data.teamCount,
-            duration: detailsResponse.data.duration,
-            matchDuration: detailsResponse.data.matchDuration
+            tournamentDuration: detailsResponse.data.tournamentDuration,
+            matchDuration: detailsResponse.data.matchDuration,
+            teams: detailsResponse.data.teams,
           });
+          
         } catch (error) {
           console.error('Error fetching tournament data:', error);
         }
@@ -42,13 +52,74 @@ const TournamentDetails = () => {
     }
   }, []);
 
+  //console.log("TOURNY INFO", tournament.teams);
+
+  useEffect(() => {
+    const user1 = JSON.parse(sessionStorage.getItem("user"));
+    setUser(user1);
+  
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`http://localhost:5000/get_teams?email=${user1.email}`);
+        if (response.data.length > 0) {
+          setUserTeams(response.data);
+          console.log("Fetched teams:", response.data);
+        } else {
+          console.log("No teams found");
+        }
+      } catch (error) {
+        console.error('Error getting teams', error);
+      }
+    };
+  
+    fetchData();
+  }, []);
+
+  const handleCheckboxChange = (teamId) => {
+     console.log("Selected team ID:", teamId); 
+    setSelectedTeamId(teamId);
+  };  
+  
+
   const handleJoinTournament = () => {
-    alert("Join Tournament logic here");
+    setIsJoinTournamentModalOpen(true);
+    //alert("Join Tournament logic here");
   };
 
   const handleLeaveTournament = () => {
     alert("Leave Tournament logic here");
   };
+
+  const joinTournamentWithTeam = async () => {
+    //if (!selectedTeamId) return;
+
+    //console.log("teamID", team._id);
+
+    console.log("Selected Team ID:", selectedTeamId);
+
+    console.log("Joining tournament with ID:", tournament.objectID, "and team ID:", selectedTeamId);
+
+  
+    try {
+      await axios.post('http://localhost:5000/join_tournament', { tournamentId: tournament.objectID, teamId: selectedTeamId });
+      setIsJoinTournamentModalOpen(false);
+      setSelectedTeamId(null);
+    } catch (error) {
+      console.error('Error joining tournament:', error);
+    }
+  };
+  
+  
+  const createAndJoinTeam = async () => {
+    try {
+      // API call to create a new team
+      const newTeam = await axios.post('http://localhost:5000/join_tournament', { name: newTeamName });
+      joinTournamentWithTeam(newTeam.data.id);
+    } catch (error) {
+      console.error('Error creating new team:', error);
+    }
+  };
+  
 
   return (
     <div className="w-full flex gap-8">
@@ -63,12 +134,14 @@ const TournamentDetails = () => {
         </p>
 
         <p className="pt-4">
-          Tournament Duration: {tournament.duration} days
+          Tournament Duration: {tournament.tournamentDuration} days
         </p>
         <p>
           Match Duration: {tournament.matchDuration} minutes
         </p>
       </div>
+
+      
 
       {/* Actions Section */}
       <div className="w-1/3 border border-gray-300 rounded-xl h-128 shadow-lg">
@@ -76,15 +149,76 @@ const TournamentDetails = () => {
           <button
             onClick={handleJoinTournament}
             className="w-full bg-green-500 hover:ease-in duration-100 text-white font-semibold text-lg rounded-xl py-2 mb-4"
+            disabled={tournament.isFull}
           >
-            Join Tournament
+            {tournament.isFull ? "Tournament Full" : "Join Tournament"}
           </button>
+          
+          {isJoinTournamentModalOpen && (
+  <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+    <div className="modal-container bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg z-50 p-6">
+      <button className="bg-red-500 text-white px-4 py-2 rounded-lg float-right" onClick={() => setIsJoinTournamentModalOpen(false)}>X</button>
+      <h1 className="text-2xl font-semibold mb-4">Join Tournament</h1>
+
+      {/* Team Selection */}
+      <div>
+        <h2 className="text-lg font-semibold mb-2">Select Your Team</h2>
+        {userTeams.map((team) => (
+  <div key={team._id} className="flex items-center mb-2">
+    <input
+      type="radio"
+      name="teamSelection"
+      value={team._id}
+      checked={selectedTeamId === team._id}
+      onChange={() => handleCheckboxChange(team._id)}
+    />
+    <label className="ml-2 cursor-pointer" onClick={() => handleCheckboxChange(team._id)}>
+      {team.name}
+    </label>
+  </div>
+))}
+
+      </div>
+
+      {/* Join with Selected Team Button */}
+      <button
+        className="bg-green-500 text-white px-4 py-2 rounded-lg w-full mt-4"
+        onClick={() => joinTournamentWithTeam()}
+        //disabled={!selectedTeamId}
+      >
+        Join with Selected Team
+      </button>
+
+      {/* Create New Team */}
+      <div className="mt-4">
+        <input
+          type="text"
+          className="w-full border mt-1 p-2 rounded-md"
+          placeholder="Enter New Team Name"
+          value={newTeamName}
+          onChange={(e) => setNewTeamName(e.target.value)}
+        />
+        <button className="bg-blue-500 text-white px-4 py-2 rounded-lg w-full mt-4" onClick={createAndJoinTeam}>Create and Join</button>
+      </div>
+    </div>
+  </div>
+)}
+
+
           <button
             onClick={handleLeaveTournament}
             className="w-full bg-red-500 text-white font-semibold text-lg rounded-xl py-2 mb-4"
           >
             Leave Tournament
           </button>
+
+          <h2 className="text-xl font-semibold mb-2">Teams in this Tournament:</h2>
+        {tournament.teams && tournament.teams.length > 0
+          ? tournament.teams.map((teamName, index) => (
+           <p key={index}>{teamName}</p>
+          ))
+          : <p>No teams have joined yet.</p>
+        }
         </div>
       </div>
     </div>
