7ae8bb525f6d650eb0ddb90676c6011208b008f5
Ani Hemige <51936458+ahemige@users.noreply.github.com>
2023-12-01 01:04:09 -0500

    Update join_event

--- env/app.py
+++ env/app.py
@@ -918,7 +918,10 @@ def join_event():
     users.update_one({"username": data.get('username')}, {
         "$set": {"numBasketball": bball, "numTennis": tennis, "numSoccer": soccer, "numWeights": weights}})
 
-    return join(data.get("id"), data.get("username"), list(events.find()))
+    team_green = data.get("teamGreen")
+    team_blue = data.get("teamBlue")
+
+    return join(data.get("id"), data.get("username"), list(events.find()), team_green, team_blue)
 
 
 def leave_event():
