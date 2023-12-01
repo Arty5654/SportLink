535299ed0a495291aed650b5f79812df45a7671c
Ani Hemige <51936458+ahemige@users.noreply.github.com>
2023-11-30 23:26:21 -0500

    Revert "Event teams"

--- web/app/events/page.js
+++ web/app/events/page.js
@@ -1,11 +1,10 @@
 "use client";
 
 import { useRouter, useSearchParams } from "next/navigation";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import User from "@app/User";
 import SmallMap from "./SmallMap";
-import eventsCSS from "./eventsCSS.css"
 
 const ParticipantCard = ({ username }) => {
   const [user, setUser] = useState(new User());
@@ -96,8 +95,6 @@ const EventDetails = () => {
     eventOwner: "",
     town: "",
     end: false,
-    teamGreen: [],
-    teamBlue: [],
   });
 
   const status = event.currentParticipants < event.maxParticipants ? "Open" : "Closed";
@@ -106,8 +103,6 @@ const EventDetails = () => {
   const searchParams = useSearchParams();
   const router = useRouter();
   const eventID = searchParams.get("id");
-  const [isMemberOfGreen, setIsMemberOfGreen] = useState(false);
-  const [isMemberOfBlue, setIsMemberOfBlue] = useState(false);
 
   useEffect(() => {
     const currentUser = JSON.parse(sessionStorage.getItem("user"));
@@ -119,7 +114,6 @@ const EventDetails = () => {
       try {
         axios.get(`http://localhost:5000/get_event_details?id=${eventID}`).then((response) => {
           const data = response.data;
-          console.log(data)
           setEvent({
             title: data.title,
             desc: data.desc,
@@ -135,15 +129,8 @@ const EventDetails = () => {
             eventOwner: data.eventOwner,
             town: data.town,
             end: data.end,
-            teamBlue: data.teamBlue,
-            teamGreen: data.teamGreen
           });
-
-          if (event.teamBlue && event.teamBlue.includes(user.username)) {
-            setIsMemberOfBlue(true);
-          } else {
-            setIsMemberOfBlue(false);
-          }
+          console.log(response.data);
         });
       } catch (error) {
         console.log("Error: ", error);
@@ -196,8 +183,6 @@ const EventDetails = () => {
               numTennis: updatedUser.numTennis,
               numSoccer: updatedUser.numSoccer,
               numWeights: updatedUser.numWeights,
-              teamGreen: event.teamGreen,
-              teamBlue: event.teamBlue
             })
             .then((response) => {
               if (response.status === 200) {
@@ -228,74 +213,6 @@ const EventDetails = () => {
   const handleEditClick = () => {
     router.push(`/edit-event?id=${eventID}`);
   };
-
-  const handleJoinTeam = (team) => {
-    const updatedEvent = { ...event };
-
-
-    if (event.teamBlue.includes(user.username)) {
-
-      alert("Already a part of Team Blue")
-      setIsMemberOfGreen(false)
-      setIsMemberOfBlue(true)
-      return;
-    }
-    if (event.teamGreen.includes(user.username)) {
-
-      alert("Already a part of Team Green")
-      setIsMemberOfGreen(true)
-      setIsMemberOfBlue(false)
-      return;
-
-    }
-    if (team === 'green') {
-      updatedEvent.teamGreen.push(user.username);
-      updatedEvent.teamBlue = updatedEvent.teamBlue.filter(email => email !== user.username);
-      setIsMemberOfGreen(true);
-      setIsMemberOfBlue(false);
-    } else if (team === 'blue') {
-      updatedEvent.teamBlue.push(user.username);
-      updatedEvent.teamGreen = updatedEvent.teamGreen.filter(email => email !== user.username);
-      setIsMemberOfBlue(true);
-      setIsMemberOfGreen(false);
-    }
-    setEvent(updatedEvent);
-    axios
-        .post("http://localhost:5000/update_lists", {
-          id: eventID,
-          teamGreen: event.teamGreen,
-          teamBlue: event.teamBlue
-        })
-        .then((response) => {
-          if (response.status === 200) {
-            console.log("Added to Team");
-          }
-        });
-  };
-
-  const handleLeaveTeam = (team) => {
-    const updatedEvent = { ...event };
-    if (team === 'green') {
-      updatedEvent.teamGreen = updatedEvent.teamGreen.filter(email => email !== user.username);
-      setIsMemberOfGreen(false);
-    } else if (team === 'blue') {
-      updatedEvent.teamBlue = updatedEvent.teamBlue.filter(email => email !== user.username);
-      setIsMemberOfBlue(false);
-    }
-    setEvent(updatedEvent);
-    axios
-        .post("http://localhost:5000/update_lists", {
-          id: eventID,
-          teamGreen: event.teamGreen,
-          teamBlue: event.teamBlue
-        })
-        .then((response) => {
-          if (response.status === 200) {
-            console.log("Added to Team");
-          }
-        });
-  };
-
 
   return (
       <div className="w-full flex gap-8">
@@ -341,31 +258,6 @@ const EventDetails = () => {
                 zoom={15}
                 address={event.address}
             />
-          </div>
-          <div className="team-container">
-            <div className="team-card green-team">
-              <h3 className="team-title">Team Green</h3>
-              {event.teamGreen.map((member, index) => (
-                  <p key={index} className="team-member">{member}</p>
-              ))}
-              {isMemberOfGreen ? (
-                  <button onClick={() => handleLeaveTeam('green')}>Leave Team</button>
-              ) : (
-                  <button onClick={() => handleJoinTeam('green')} disabled={isMemberOfBlue}>Join Team</button>
-              )}
-            </div>
-
-            <div className="team-card blue-team">
-              <h3 className="team-title">Team Blue</h3>
-              {event.teamBlue.map((member, index) => (
-                  <p key={index} className="team-member">{member}</p>
-              ))}
-              {isMemberOfBlue ? (
-                  <button onClick={() => handleLeaveTeam('blue')}>Leave Team</button>
-              ) : (
-                  <button onClick={() => handleJoinTeam('blue')} disabled={isMemberOfGreen}>Join Team</button>
-              )}
-            </div>
           </div>
         </div>
 
